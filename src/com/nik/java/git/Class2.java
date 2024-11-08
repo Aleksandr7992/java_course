ed06a112cf449fdc8fe13f9feb319faaee2dabd8
Bor <stat7992@gmail.com>
2024-11-08 09:40:55 +0300

    The third commit

--- src/com/nik/java/git/Class2.java
+++ src/com/nik/java/git/Class2.java
@@ -3,6 +3,7 @@ package com.nik.java.git;
 public class Class2 {
     public static void main(String[] args) {
         System.out.println("Hello");
+        System.out.println("Second line");
     }
 
 
